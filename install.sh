ec3bf7829698c9a63f2ec17bddd9396095de73cb
AzreXCyBerTech <maellambong2219@gmail.com>
2018-05-11 05:54:37 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+wget https://raw.githubusercontent.com/maellambong/-x-DDos/master/tool/hammer.py
+wget https://raw.githubusercontent.com/maellambong/-x-DDos/master/tool/torshammer.py
+wget https://raw.githubusercontent.com/maellambong/-x-DDos/master/tool/xerxes.c
+wget https://raw.githubusercontent.com/maellambong/-x-DDos/master/tool/ddos.py
+wget https://raw.githubusercontent.com/maellambong/-x-DDos/master/tool/ddos2.py
+wget https://raw.githubusercontent.com/maellambong/-x-DDos/master/tool/Anonymous.pl
