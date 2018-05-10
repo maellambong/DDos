ae0b15ce5332a5628336a5bdc5595f4e8190f324
AzreXCyBerTech <maellambong2219@gmail.com>
2018-05-11 06:40:22 +0800

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-wget https://raw.githubusercontent.com/maellambong/-x-DDos/master/tool/hammer.py
-wget https://raw.githubusercontent.com/maellambong/-x-DDos/master/tool/torshammer.py
-wget https://raw.githubusercontent.com/maellambong/-x-DDos/master/tool/xerxes.c
-wget https://raw.githubusercontent.com/maellambong/-x-DDos/master/tool/ddos.py
-wget https://raw.githubusercontent.com/maellambong/-x-DDos/master/tool/ddos2.py
-wget https://raw.githubusercontent.com/maellambong/-x-DDos/master/tool/Anonymous.pl
